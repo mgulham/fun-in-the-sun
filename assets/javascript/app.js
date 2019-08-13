ae8d384a42dd16ebb38298f75e7abfebb5769259
mgulham <mgulham@yahoo.com>
2019-08-13 15:52:21 -0700

    Form clear test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,19 +22,20 @@ var minTemp = 0
 
 $("#add-info-btn").on("click", function(event) {
   event.preventDefault();
- 
+  
 
   // This line grabs the input from the textbox
   
     zipCode = $("#zip-name-input").val().trim();
     skinType = $("#exampleFormControlSelect1").val();
    
-    $(".my-form").empty()
+    // $(".my-form").empty()
     $(".my-form").html("<h3>Check the Console</h3>")
-let owQueryUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${owApiKey}` 
-  
-// Creating an AJAX call for the specific button being clicked
-        
+
+
+    // Creating an AJAX call when form submitted
+    let owQueryUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${owApiKey}` 
+
         $.ajax({
           url: owQueryUrl,
           method: "GET"
